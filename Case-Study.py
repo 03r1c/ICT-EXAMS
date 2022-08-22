12bb26751fd70499e2fbea7d447f9a70c136016b
03r1c <106050054+03r1c@users.noreply.github.com>
2022-08-22 16:48:37 +0700

    Create Case-Study.py

---
+++ Case-Study.py
@@ -0,0 +1,12 @@
+def sum(apple, kiwi, banana):
+    return (apple + kiwi + banana)
+a = 1853
+b = 1000
+apple = int('213')
+kiwi = int('475')
+banana = int('1165')
+
+if a > b:
+  print("Siska money is not enough")
+
+print(f'apple : {apple} + kiwi : {kiwi} + banana : {banana} so total money that siska need is {sum(apple, kiwi, banana)}')
